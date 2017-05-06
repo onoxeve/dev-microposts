0f4cbc4d8af1ac904f5f0bc9e09f15060ca2b4da
onoxeve <onoxeve@gmail.com>
2017-05-06 08:15:23 +0000

    add favorites

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,5 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
   
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,11 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
-  
+
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
+
+  # follow/unfollow
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -29,6 +33,20 @@ class User < ApplicationRecord
 
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+
+  # like/unlike
+  def like(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+
+  def unlike(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+
+  def like?(micropost)
+    self.likes.include?(micropost)
   end
 
 end
